663591c0dbabbdeb4acb7ca6668379486d3ba3e5
Matheus Felipe Araujo <matheus151929@live.com>
2025-08-21 13:07:46 -0400

    Streams com paralelismo

--- src/Streams.java
+++ src/Streams.java
@@ -2,15 +2,28 @@ import java.util.ArrayList;
 
 public class Streams {
     
+    // Paralelismo: capacidade de executar múltiplas tarefas simultaneamento em vários processadores.
     
 
+    // Sem paralelismo
      public static void main(String... params) throws Exception{
         ArrayList<Object> livros = new ArrayList<>();
 
+        // sempre deveolverá na mesma ordem sequencial
         livros
         .stream()
         .filter(livro -> livro.toString().toLowerCase().contains("a") || livro.toString().toLowerCase().contains("o")) // métodos eager
         .forEach(System.out::println);  // métodos consumo
+
+
+        // Com paralelismo
+        // Não obedece a ordem, resultado de ordem variável.
+        // Ordem não é previsível pois está sendo executado
+        // em uma Thread separada usando core do processador do hardware.
+        livros
+        .parallelStream()
+        .filter(livro -> livro.toString().toLowerCase().contains("a") || livro.toString().toLowerCase().contains("o"))
+        .forEach(System.out::println);
     }
 
 
